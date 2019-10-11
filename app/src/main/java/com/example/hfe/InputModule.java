5b2f7e4810aa89590ee9abc26039a0b08d02d04e
bhavyameghnani <meghnani.bhavya@gmail.com>
2019-10-11 23:19:32 +0530

    Activities added : Input Module & Maps

---
+++ app/src/main/java/com/example/hfe/InputModule.java
@@ -0,0 +1,14 @@
+package com.example.hfe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputModule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_module);
+    }
+}
